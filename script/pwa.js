137e7dd4b6468488edcb151e008f940db9f19034
anandbaraik <anandbaraik2014@gmail.com>
2022-03-02 22:58:51 +0530

    fix:service worker registeration issue :ambulance:

--- script/pwa.js
+++ script/pwa.js
@@ -40,7 +40,7 @@ function addToHomeScreen() {
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
         navigator.serviceWorker
-        .register("./serviceWorker.js")
+        .register("./script/serviceWorker.js")
         .then(res => console.log("service worker registered"))
         .catch(err => console.log("service worker not registered", err));
     });
